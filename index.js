ee3129af14080d07465ddc65b027c51fb04ee7d5
Liam Gibbs <liamsamuelgibbs@hotmail.com>
2025-10-20 16:42:27 +0100

    Push messages to the queue

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
+const fplQueueUrl = 'http://localhost:5001/publish/fpl'
+const whatsAppQueue = 'whatsapp.response'
+
+async function postMessage(action) {
+  try {
+    const response = await fetch(fplQueueUrl, {method: "POST", body: JSON.stringify({action: action, reply_to: whatsAppQueue})})
+    
+    if(!response.ok) {
+      throw new Error(response.status)
+    }
+    
+    const result = await response.json()
+    console.log(result)
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+async function getMessage() {}
+
 const client = new Client(
   {  
       puppeteer: {
@@ -23,10 +43,15 @@ client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-client.on('message', msg => {
+client.on('message_create', async msg => {
     if (msg.body == '!ping') {
         msg.reply('pong');
     }
+    
+    if (msg.body.startsWith('!')) {
+      await postMessage(msg.body)
+    }
+    
 });
 
 client.initialize();
